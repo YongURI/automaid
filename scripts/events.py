1dca4b1186dbb9aec82074a4f4b494c175d503e5
YongURI <37390218+YongURI@users.noreply.github.com>
2020-05-11 10:01:51 +0800

    Update events.py
    if the date of the event is as ['2020-03-16T01:06:42'] without millisecond, the script will break.

--- scripts/events.py
+++ scripts/events.py
@@ -69,6 +69,8 @@ class Event:
             self.requested = False
             self.trig = int(catch_trig[0])
             date = re.findall(" DATE=(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6})", header, re.DOTALL)
+                if [date] = []:
+                    date = re.findall(" DATE=(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})", header, re.DOTALL)
             self.date = UTCDateTime(date[0])
             self.depth = int(re.findall(" PRESSURE=(\d+)", self.header)[0])
             self.temperature = int(re.findall(" TEMPERATURE=(-?\d+)", self.header)[0])
